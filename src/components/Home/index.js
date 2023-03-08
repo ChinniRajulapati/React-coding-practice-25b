a1955871fccdd7625912a481ccb689489c6e08fd
ChinniRajulapati <rchinni98@gmail.com>
2023-03-08 05:55:43 +0000

    first commit

---
+++ src/components/Home/index.js
@@ -0,0 +1,13 @@
+import UserInfo from '../UserInfo/index'
+import BlogList from '../BlogList'
+
+import './index.css'
+
+const Home = () => (
+  <div className="home-container">
+    <UserInfo />
+    <BlogList />
+  </div>
+)
+
+export default Home
